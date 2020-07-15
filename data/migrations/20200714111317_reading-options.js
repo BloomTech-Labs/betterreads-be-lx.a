d245c813e1eafa1c54cc849c2f2ece3ef26758d7
Heather Nuffer <zzzuzz@gmail.com>
2020-07-15 09:49:57 -0600

    Chore: add reading-options seeds and Fix: change field type of status in reading-types migration
    Co-authored-by: samjeffcoat <samjeffcoat@gmail.com>
    Co-authored-by: modernAdmiral <jackson.mccomas@gmail.com>

--- data/migrations/20200714111317_reading-options.js
+++ data/migrations/20200714111317_reading-options.js
@@ -1,7 +1,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable('reading_options', function (table) {
     table.increments('id').notNullable().unique().primary();
-    table.integer('status').notNullable();
+    table.string('status').notNullable();
   });
 };
 
