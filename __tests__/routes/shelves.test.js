a20238e91f3fd36523b61c7461c55a01be248377
Heather Nuffer <zzzuzz@gmail.com>
2020-07-22 15:02:44 -0600

    Chore: Adds test for GET shelf by id endpoint

--- __tests__/routes/shelves.test.js
+++ __tests__/routes/shelves.test.js
@@ -22,4 +22,28 @@ describe('shelves router endpoints', () => {
       expect(Shelves.findAll.mock.calls.length).toBe(1);
     });
   });
+
+  describe('GET /shelves/:id', () => {
+    it('should return 200 when shelf found', async () => {
+      Shelves.findById.mockResolvedValue({
+        id: 20,
+        name: 'chick-lit',
+        profileId: 2,
+      });
+      const res = await request(server).get('/shelves/20');
+
+      expect(res.status).toBe(200);
+      expect(res.body.name).toBe('chick-lit');
+      expect(res.body.profileId).toBe(2);
+      expect(Shelves.findById.mock.calls.length).toBe(1);
+    });
+
+    it('should return 404 when no shelf found', async () => {
+      Shelves.findById.mockResolvedValue();
+      const res = await request(server).get('/shelves/20');
+
+      expect(res.status).toBe(404);
+      expect(res.body.error).toBe('ShelfNotFound');
+    });
+  });
 });
