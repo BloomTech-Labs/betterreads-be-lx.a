33568a5d5853e1c313a4734ff4dbdf1b1cc40ab0
Heather Nuffer <zzzuzz@gmail.com>
2020-07-23 13:18:59 -0600

    Feat: Allows the num of results and the start index to be specified for searching

--- api/routes/search.js
+++ api/routes/search.js
@@ -2,15 +2,29 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
+// In request body, query is required. startIndex and maxResults are optional.
+// Default maxResults is 10.
+// Default startIndex is 0.
 router.get('/', (req, res) => {
   let queryBody = req.body.query;
+  const startIndex = req.body.startIndex;
+  let maxResults = req.body.maxResults;
   queryBody = queryBody.replace(/ /g, '+');
-  const query = 'https://www.googleapis.com/books/v1/volumes?q=' + queryBody;
+  let query = 'https://www.googleapis.com/books/v1/volumes?q=' + queryBody;
+  if (startIndex) {
+    query += '&startIndex=' + startIndex;
+  }
+  if (maxResults > 40) {
+    maxResults = 40;
+  }
+  if (maxResults) {
+    query += '&maxResults=' + maxResults;
+  }
   console.log(query);
   axios
     .get(query)
     .then((response) => {
-      const firstTenParsed = response.data.items.map((book) => {
+      const responseParsed = response.data.items.map((book) => {
         return {
           googleId: book.id,
           title: book.volumeInfo.title,
@@ -22,7 +36,7 @@ router.get('/', (req, res) => {
       res.status(200).json({
         totalItems: response.data.totalItems,
         returnedItemsLength: response.data.items.length,
-        firstTen: firstTenParsed,
+        results: responseParsed,
       });
     })
     .catch((err) => {
